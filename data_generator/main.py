cc8b2b0cd02df6da6eb7155877f8e3539d8580ce
milosz-student <milosz.ilecki@gmail.com>
2023-12-21 22:21:03 +0100

    data generator

--- data_generator/main.py
+++ data_generator/main.py
@@ -33,7 +33,7 @@ def new_sensor_reading(sensor_id, sensor_type, value):
         "sensor_id": sensor_id,
         "type": sensor_type,
         "value": value,
-        "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "timestamp": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
     }
 
 def main():
