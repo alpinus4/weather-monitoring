3d6eb456ba1d7ea70aea7d547e4472e3266d2d35
milosz-student <milosz.ilecki@gmail.com>
2023-12-21 22:17:58 +0100

    delete endpoints

--- webapp/ClientApp/src/AppRoutes.js
+++ webapp/ClientApp/src/AppRoutes.js
@@ -1,17 +1,8 @@
-import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
-import { Home } from "./components/Home";
 
 const AppRoutes = [
   {
     index: true,
-    element: <Home />
-  },
-  {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
     path: '/fetch-data',
     element: <FetchData />
   }
