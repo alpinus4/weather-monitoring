6b3d07def09aa9adbff04df2c8e1530ba7dffd37
milosz-student <milosz.ilecki@gmail.com>
2023-12-21 22:15:27 +0100

    A Filters class has been created

---
+++ webapp/Model/Filter.cs
@@ -0,0 +1,13 @@
+﻿namespace webapp.Model;
+
+
+public class Filters
+{
+    public DateTime? dateFrom { get; set; }
+    public DateTime? dateTo { get; set; }
+    public List<string>? types { get; set; }
+    public List<int>? ids { get; set; }
+    public int? valueFrom { get; set; }
+    public int? valueTo { get; set; }
+}
+
