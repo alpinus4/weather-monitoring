09aa45aff5e94505168ddcb92bc38112a1a7c63c
milosz-student <milosz.ilecki@gmail.com>
2023-12-21 22:15:41 +0100

    A Data class has been created

---
+++ webapp/Model/Data.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace webapp.Model;
+
+public class Data
+{
+
+    [BsonId]
+    public ObjectId _id { get; set; }
+    public string? type { get; set; }
+    public int sensor_id { get; set; }
+    public DateTime timestamp { get; set; }
+    public double value { get; set; }
+}
