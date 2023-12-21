5f634d4ab755560cecf30c0708d4515fcbb5f2d7
milosz-student <milosz.ilecki@gmail.com>
2023-12-21 22:09:44 +0100

    The navigation menu has been fixed, and unnecessary fields have been removed

--- webapp/ClientApp/src/components/NavMenu.js
+++ webapp/ClientApp/src/components/NavMenu.js
@@ -25,16 +25,10 @@ export class NavMenu extends Component {
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">webapp</NavbarBrand>
+          <NavbarBrand tag={Link} to="/fetch-data">weather-monitoring</NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
             <ul className="navbar-nav flex-grow">
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-              </NavItem>
               <NavItem>
                 <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
               </NavItem>
